b7a5f06aa8579a45b030aeab5e2b0e1de04a2db0
Sharique Asghar <sharique@fisdom.com>
2021-07-04 23:22:51 +0530

    removed unnecessary code

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,8 +1,3 @@
-import BusinessIcon from '@material-ui/icons/Business';
-import MovieFilterIcon from '@material-ui/icons/MovieFilter';
-import SportsIcon from '@material-ui/icons/Sports';
-import WebIcon from '@material-ui/icons/Web';
-
 export const API_CONSTANTS = {
   getTopHeadlines: '/api/top-headlines',
   getAnyNews: '/api/everything'
@@ -15,9 +10,9 @@ export const sidebarCategoryList = {
   //   { id: 2, title: "India", code: "in" }
   // ],
   category: [
-    { id: 0, title: "Business", value: "business", icon: BusinessIcon },
-    { id: 1, title: "Entertainment", value: "entertainment", icon: MovieFilterIcon },
-    { id: 2, title: "Sports", value: "sports", icon: SportsIcon },
-    { id: 3, title: "Technology", value: "technology", icon: WebIcon }
+    { id: 0, title: "Business", value: "business" },
+    { id: 1, title: "Entertainment", value: "entertainment" },
+    { id: 2, title: "Sports", value: "sports" },
+    { id: 3, title: "Technology", value: "technology" }
   ],
 }
